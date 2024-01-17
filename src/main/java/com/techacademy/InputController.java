27186468f78d27ee0acf6534821591cdf717cca5
ymkk0811 <ymkk.hana@docomo.ne.jp>
2024-01-17 20:26:11 +0900

    課題 入力フォームの内容を画面に表示しよう

---
+++ src/main/java/com/techacademy/InputController.java
@@ -0,0 +1,19 @@
+package com.techacademy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class InputController {
+    //メソッド名: getInput
+    //処理内容: 入力画面の初期表示をする
+    @GetMapping("/input")
+    public String getInput(@RequestParam(name = "previous", required = false) String previous, Model model) {
+        model.addAttribute("previous", previous);
+        // input.htmlに画面遷移
+        return "input";
+    }
+}
